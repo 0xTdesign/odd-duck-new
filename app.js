3f8c863a70860a754d959ee4480c5c52084d940c
jimbobdxb <tallisondxb@gmail.com>
2022-09-06 12:06:51 +0400

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Global Variables
+let duckContainer = document.querySelector("main");
 let result = document.getElementById("results");
 let button = document.getElementById("btn");
 let image1 = document.getElementById("image1");
@@ -8,38 +9,94 @@ let image3 = document.getElementById("image3");
 let click = 0; // This is used for click count
 let maxClick = 3; // This is max clicks the user is allowed
 
-function oddDuck(name, src) {
+function OddDuck(name, src) {
   // Constructor with key value pairs.
   this.name = name;
   this.src = src;
   this.click = 0;
   this.views = 0;
 
-  oddDuck.allDuckArray.push(this); // This code runs every time the constructor is called
+  OddDuck.allDuckArray.push(this); // This code runs every time the constructor is called
 }
 
-oddDuck.allDuckArray = []; //
+OddDuck.allDuckArray = []; //
 
 function getRandomNum() {
-  return Math.floor(Math.random() * oddDuck.length); // This creates a random number for the images
+  return Math.floor(Math.random() * OddDuck.allDuckArray.length); // This creates a random number for the images
 }
 
-new oddDuck("bag", "./img/bag.jpeg");
-new oddDuck("banana", "./img/banana.jpeg");
-new oddDuck("bathroom", "./img/bathroomn.jpeg");
-new oddDuck("boots", "./img/boots.jpeg");
-new oddDuck("breafast", "./img/breakfast.jpeg");
-new oddDuck("bubblegum", ".img/bubblegum.jpeg");
-new oddDuck("chair", "./img/chair.jpeg");
-new oddDuck("cthulhu", "./img/cthulhu.jpeg");
-new oddDuck("dog-duck", "./img/dog-duck.jpeg");
-new oddDuck("dragon", "./img/dragon.jpeg");
-new oddDuck("pen", "./img/pen.jpeg");
-new oddDuck("pet-sweep", "./img/pet-sweep.jpeg");
-new oddDuck("scissors", "./img/scissors.jpeg");
-new oddDuck("shark", "./img/shark.jpeg");
-new oddDuck("sweep", "./img/sweep.jpeg");
-new oddDuck("tauntaun", "./img/tauntaun.jpeg");
-new oddDuck("unicorn", "./img/unicorn.jpeg");
-new oddDuck("water-can", "./img/water-can.jpeg");
-new oddDuck("wine-glass", "./img/wine-glass.jpeg");
+// Render image to a numnber and check them against each other so cant get the same image
+
+function renderOddDuck() {
+  let duck1 = getRandomNum();
+  let duck2 = getRandomNum();
+  let duck3 = getRandomNum();
+
+  while (image1 === image2 && image2 === image3) {
+    console.log("Keeps looping the image count");
+    image1 = getRandomNum();
+    image2 = getRandomNum();
+    image3 = getRandomNum();
+  }
+
+  //Set Image attributes for our images
+  image1.src = OddDuck.allDuckArray[duck1].src; // This sets the image
+  image2.src = OddDuck.allDuckArray[duck2].src; // ''
+  image3.src = OddDuck.allDuckArray[duck3].src; // ''
+  image1.alt = OddDuck.allDuckArray[duck1].name; // This sets the name of the pic
+  image2.alt = OddDuck.allDuckArray[duck2].name; // ''
+  image3.alt = OddDuck.allDuckArray[duck3].name; // ''
+
+  OddDuck.allDuckArray[duck1].views++; // Increase the views when rendered
+  OddDuck.allDuckArray[duck2].views++;
+  OddDuck.allDuckArray[duck3].views++;
+}
+
+//  Now to do Button function
+
+function buttonClick(event) {
+  if (event.target === duckContainer) {
+    alert("Please only click the images");
+  }
+  click++;
+  let clickDuck = event.target.alt;
+
+  for (let i = 0; i < OddDuck.allDuckArray.length; i++) {
+    if (clickDuck === OddDuck.allDuckArray[i].name) {
+      OddDuck.allDuckArray[i].clicks++;
+      break;
+    }
+  }
+  if (clicks === maxClick) {
+    // Once the clicks have reached max clicks then it will stop the image click
+    duckContainer.removeEventListener("click", buttonClick);
+
+    // Now the button will light up to be able to click .
+    button.addEventListener("click", renderResults);
+    // Style button
+    button.className = "clicks-allowed";
+    duckContainer.classname = "not-voting";
+  }
+}
+
+new OddDuck("bag", "./img/bag.jpeg");
+new OddDuck("banana", "./img/banana.jpeg");
+new OddDuck("bathroom", "./img/bathroomn.jpeg");
+new OddDuck("boots", "./img/boots.jpeg");
+new OddDuck("breafast", "./img/breakfast.jpeg");
+new OddDuck("bubblegum", ".img/bubblegum.jpeg");
+new OddDuck("chair", "./img/chair.jpeg");
+new OddDuck("cthulhu", "./img/cthulhu.jpeg");
+new OddDuck("dog-duck", "./img/dog-duck.jpeg");
+new OddDuck("dragon", "./img/dragon.jpeg");
+new OddDuck("pen", "./img/pen.jpeg");
+new OddDuck("pet-sweep", "./img/pet-sweep.jpeg");
+new OddDuck("scissors", "./img/scissors.jpeg");
+new OddDuck("shark", "./img/shark.jpeg");
+new OddDuck("sweep", "./img/sweep.jpeg");
+new OddDuck("tauntaun", "./img/tauntaun.jpeg");
+new OddDuck("unicorn", "./img/unicorn.jpeg");
+new OddDuck("water-can", "./img/water-can.jpeg");
+new OddDuck("wine-glass", "./img/wine-glass.jpeg");
+
+renderOddDuck(); // Final render
